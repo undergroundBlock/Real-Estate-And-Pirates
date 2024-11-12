3a342271415e37875ee123303508d81ddd4f696c
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2024-11-12 22:01:04 +0200

    sss7

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="./vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -59,7 +59,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="./file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -74,7 +74,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="./window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -89,7 +89,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="./globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
