6fcb912630a7f1d57a2b3738cd03cebea9dfa0e4
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2024-11-13 01:24:20 +0200

    added text 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import useTelegramUser from "@/hooks/useTelegramUser";
 import Image from "next/image";
 // const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
 // const prefix = 'https://undergroundblock.github.io/Real-Estate-And-Pirates';
+
+
 export default function Home() {
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -16,9 +18,8 @@ export default function Home() {
           priority
         />
         <strong>Real Estate and pirates</strong>
-        <p>{useTelegramUser()?.id}</p>
-        <p>{useTelegramUser()?.last_name}</p>
-        <p>{useTelegramUser()?.first_name}</p>
+
+
         </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
--- hooks/useTelegramUser.ts
+++ hooks/useTelegramUser.ts
@@ -1,30 +1,30 @@
-"use client"
+// "use client"
 
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-type TelegramUser = {
-    id: number;
-    first_name?: string;
-    last_name?: string;
-    username?: string;
-    photo_url?: string;
-    language_code?: string;
-  };
+// type TelegramUser = {
+//     id: number;
+//     first_name?: string;
+//     last_name?: string;
+//     username?: string;
+//     photo_url?: string;
+//     language_code?: string;
+//   };
 
-const useTelegramUser = () => {
-  const [user, setUser] = useState<TelegramUser | null>(null);
+// const useTelegramUser = () => {
+//   const [user, setUser] = useState<TelegramUser | null>(null);
 
-  useEffect(() => {
-    if ((window as any).Telegram?.WebApp) {
-      const initData = (window as any).Telegram.WebApp.initDataUnsafe;
+//   useEffect(() => {
+//     if ((window as any).Telegram?.WebApp) {
+//       const initData = (window as any).Telegram.WebApp.initDataUnsafe;
 
-      if (initData?.user) {
-        setUser(initData.user);
-      }
-    }
-  }, []);
+//       if (initData?.user) {
+//         setUser(initData.user);
+//       }
+//     }
+//   }, []);
 
-  return user;
-};
+//   return user;
+// };
 
-export default useTelegramUser;
+// export default useTelegramUser;
