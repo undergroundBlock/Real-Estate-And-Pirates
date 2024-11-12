31de9899f0f4ed642b6bad3e6557961413435594
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2024-11-12 20:41:50 +0200

    sd2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/Real-Estate-And-Pirates',
+  images: {
+    unoptimized: true,
+  },
 };
+
+
 
 
 export default nextConfig;
