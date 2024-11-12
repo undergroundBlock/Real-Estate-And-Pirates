36c27036d16515cd7c69c67fc38aeda7056f925e
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2024-11-12 22:37:15 +0200

    added text

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div>
         <Image
           className="dark:invert"
           src="./pirates.svg"
@@ -13,6 +14,8 @@ export default function Home() {
           height={38}
           priority
         />
+        <strong>Real Estate and pirates</strong>
+        </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
