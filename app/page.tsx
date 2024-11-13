9ad9b4c84a01465ca57f0666480559f42cb8863c
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2024-11-13 04:08:39 +0200

    added text 5

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+import WebApp from "@twa-dev/sdk";
 import Image from "next/image";
-// const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+import { useEffect, useState } from "react";
 // const prefix = 'https://undergroundblock.github.io/Real-Estate-And-Pirates';
+
+interface userData {
+  id: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  language_code: string;
+  is_premium: boolean;
+}
 
 export default function Home() {
   
+  const [userData, setUserData] = useState<userData | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && WebApp.initDataUnsafe.user) {
+      setUserData(WebApp.initDataUnsafe.user as userData);
+    }
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -18,7 +38,14 @@ export default function Home() {
         />
         <strong>Real Estate and pirates</strong>
         
-
+        <div>
+        {(typeof window !== 'undefined') && userData ? (
+          <p>User ID: {userData.id}</p>
+          ): (
+          <p>Not logged in</p>
+          )
+        }
+        </div>
         </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
